740cfcf7e253de2ce529f4293f8b5c9801c948e9
David Cruz <davidcruz72@gmail.com>
2019-10-30 09:44:41 -0700

    Intial commit

---
+++ scanport.go
@@ -0,0 +1,99 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"net"
+	"os"
+	"regexp"
+	"strings"
+	"time"
+)
+
+type arrayFlag []string
+
+func (i *arrayFlag) String() string {
+	return fmt.Sprintf("%s", *i)
+}
+
+func (i *arrayFlag) Set(value string) error {
+	*i = append(*i, value)
+	return nil
+}
+
+func main() {
+	var hosts arrayFlag
+	var ports arrayFlag
+
+	flag.Var(&hosts, "host", "Hostname or IP address of host to scan. Flag can be specified more than once.")
+	flag.Var(&ports, "p", "TCP/UDP port to scan. Flag can be specified more than once.")
+	protocol := flag.String("proto", "tcp", "Protocol to scan (TCP/UDP).")
+	t := flag.String("t", "1", "Connection timeout in seconds.")
+
+	flag.Parse()
+	// if flag.NArg() == 0 {
+	// 	flag.Usage()
+	// 	os.Exit(1)
+	// }
+
+	// TODO: Check if hosts and ports are defined, exit with error if not
+
+	timeout, _ := time.ParseDuration(*t + "s")
+
+	for _, h := range hosts {
+		addr, hostname := resolveHost(h)
+
+		for _, p := range ports {
+			r := scanPort(addr, p, strings.ToLower(*protocol), timeout)
+			fmt.Printf("%v (%v) ==> %v/%v is %v\n", addr, hostname, strings.ToUpper(*protocol), p, r)
+		}
+
+	}
+}
+
+func resolveHost(host string) (addr, hostname string) {
+	if isIPAddress(host) {
+		addr = host
+		r, err := net.LookupAddr(addr)
+		if err == nil {
+			// Use first returned hostname and trim trailing period
+			hostname = r[0][:len(r[0])-1]
+		} else {
+			hostname = ""
+		}
+	} else {
+		hostname = host
+		r, err := net.LookupIP(hostname)
+		if err != nil {
+			fmt.Printf("Unable to resolve host: %v\n", hostname)
+			os.Exit(1)
+		} else {
+			// Use first returned address
+			addr = r[0].String()
+		}
+	}
+	return addr, hostname
+}
+
+func isIPAddress(addr string) bool {
+	re := regexp.MustCompile(`(\d{1,3}\.){3}\d{1,3}`)
+	return re.MatchString(addr)
+}
+
+func scanPort(ip string, port string, protocol string, timeout time.Duration) string {
+	// TODO: Implment UDP port testing
+
+	target := fmt.Sprintf("%s:%s", ip, port)
+	conn, err := net.DialTimeout(protocol, target, timeout)
+
+	if err != nil {
+		if strings.Contains(err.Error(), "too many open files") {
+			time.Sleep(timeout)
+			scanPort(ip, port, protocol, timeout)
+		}
+		return "closed"
+	}
+
+	conn.Close()
+	return "open"
+}
